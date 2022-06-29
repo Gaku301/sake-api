06dd6f1c128726ad3ebdbbd18748496a03f99b3a
Gaku Ishikawa <gaku.jp.301@gmail.com>
2022-06-30 07:08:22 +0900

    create sake posts tables

---
+++ src/database/migrations/2022_06_29_214214_create_sakes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sakes', function (Blueprint $table) {
+            $table->id();
+            $table->string('sake_name', 128);
+            $table->string('kuramoto', 128);
+            $table->string('prefectur', 5);
+            $table->boolean('deleted')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sakes');
+    }
+};
