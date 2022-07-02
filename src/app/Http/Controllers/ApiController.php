bacf1031b1d22220b82bfc82eee446f156069df3
Gaku Ishikawa <gaku.jp.301@gmail.com>
2022-07-02 10:07:09 +0900

    change config and return posts at index

--- src/app/Http/Controllers/ApiController.php
+++ src/app/Http/Controllers/ApiController.php
@@ -2,14 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class ApiController extends Controller
 {
     public function index()
     {
-        return response()->json([
-            'message' => 'Success'
-        ]);
+        try {
+            $posts = Post::all();
+            $result = [
+                'message' => 'Success',
+                'posts' => $posts
+            ];
+        } catch (Exception $e) {
+            // output logs
+            Log::error(__METHOD__. ' Exception Error');
+            Log::error($e->getMessage());
+
+            $result = ['message' => 'Failed'];
+        }
+
+        return response()->json($result);
     }
 }
