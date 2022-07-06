ee171038c6b95906aafe8917071a16da2110bc43
Gaku Ishikawa <gaku.jp.301@gmail.com>
2022-07-07 06:35:19 +0900

    get user information

--- src/app/Http/Controllers/ApiController.php
+++ src/app/Http/Controllers/ApiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\Sake;
 use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
@@ -10,6 +11,9 @@ use Illuminate\Support\Facades\Log;
 
 class ApiController extends Controller
 {
+    /**
+     * Get all posts at Top screen
+     */
     public function index()
     {
         try {
@@ -18,17 +22,63 @@ class ApiController extends Controller
                 'user_name' => User::select('user_name')
                     ->whereColumn('id', 'posts.user_id')
                     ->limit(1),
+                'kuramoto' => Sake::select('kuramoto')
+                    ->whereColumn('id', 'posts.sake_id')
+                    ->limit(1)
             ])->get();
             $result = [
+                'status' => true,
                 'message' => 'Success',
-                'posts' => $posts
+                'result' => [
+                    'posts' => $posts
+                ]
             ];
         } catch (Exception $e) {
             // output logs
             Log::error(__METHOD__. ' Exception Error');
             Log::error($e->getMessage());
 
-            $result = ['message' => 'Failed'];
+            $result = [
+                'status' => false,
+                'message' => 'Failed'
+            ];
+        }
+
+        return response()->json($result);
+    }
+
+    /**
+     * Get user information at UserProfile screen
+     * @param string $user_id 
+     */
+    public function getUserInfo(string $user_id)
+    {
+        try {
+            // Get not deleted user's information
+            $user = User::where([
+                ['user_id', $user_id],
+                ['deleted', false]
+            ])->first();
+
+            // Get posts the user have
+            $posts = $user->posts;
+            $result = [
+                'status' => true,
+                'message' => 'Success',
+                'result' => [
+                    'user' => $user,
+                    'posts' => $posts
+                ]
+            ];
+        } catch (Exception $e) {
+            // output logs
+            Log::error(__METHOD__. ' Exception Error');
+            Log::error($e->getMessage());
+
+            $result = [
+                'status' => false,
+                'message' => 'Failed'
+            ];
         }
 
         return response()->json($result);
--- src/routes/api.php
+++ src/routes/api.php
@@ -22,4 +22,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->controller(ApiController::class)->group(function() {
     Route::get('/index', 'index');
+    Route::get('/user/{user_id}', 'getUserInfo');
 });
